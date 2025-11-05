e7a92d66f90c6b30e1c246c2d70206c0187eaac5
NishantPohekar <pohekarnishant14@gmail.com>
2025-11-06 02:22:28 +0530

    2nd Part completed - UI design

---
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+function Conversation() {
+  return (
+    <div>
+        <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+            <div className="avatar online">
+                <div className='w-12 rounded-full '>
+                    <img src="" alt="user avatar" />
+                </div>
+            </div>
+
+            <div className="flex flex-col flex-1 ">
+                <div className='flex gap-3'>
+                    <p className='font-bold text-gray-200 '>John Doe</p>
+                    <span className='text-xl '>🌞</span>
+                </div>
+            </div>
+        </div>
+        <div className='divider my-0 py-0 h-1 '/>
+    </div>
+  )
+}
+
+export default Conversation
+
+//STARTER CODE
+// import React from 'react'
+
+// function Conversation() {
+//   return (
+//     <div>
+//         <div className="flex gap-2 items-center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer">
+//             <div className="avatar online">
+//                 <div className='w-12 rounded-full '>
+//                     <img src="" alt="user avatar" />
+//                 </div>
+//             </div>
+
+//             <div className="flex flex-col flex-1 ">
+//                 <div className='flex gap-3'>
+//                     <p className='font-bold text-gray-200 '>John Doe</p>
+//                     <span className='text-xl '>🌞</span>
+//                 </div>
+//             </div>
+//         </div>
+//         <div className='divider my-0 py-0 h-1 '/>
+//     </div>
+//   )
+// }
+
+// export default Conversation
---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import Conversation from './Conversation'
+
+
+
+function Conversations() {
+  return (
+    <div className='py-2 flex flex-col overflow-auto'>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+        <Conversation/>
+
+        
+    </div>
+  )
+}
+
+export default Conversations
+
+// STARTER CODE
+// import React from 'react'
+// import Conversation from './Conversation'
+
+
+
+// function Conversations() {
+//   return (
+//     <div className='py-2 flex flex-col overflow-auto'>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+//         <Conversation/>
+
+        
+//     </div>
+//   )
+// }
+
+// export default Conversations
