50f7cd05d5d34e8d355e4c70d9c302fedf9a4fd8
NishantPohekar <pohekarnishant14@gmail.com>
2025-11-08 19:34:29 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path"
 
 import dotenv from "dotenv"
 dotenv.config()
@@ -16,10 +17,12 @@ app.use(express.json())
 app.use(cookieParser())
 
 
+const __dirname = path.resolve() 
 
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import userRoutes from "./routes/user.routes.js"
+
 
 
 console.log("Loaded MONGO_URL:", process.env.MONGO_URL);
@@ -28,15 +31,18 @@ import connectToMongoDb from "./db/connecttoMongoDb.js"
 
 const PORT = process.env.PORT || 5001
 
-app.get("/",(req,res)=>{
-    // route route http://localhost:5001/
-    res.send("Hello World!!")
 
-})
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/users",userRoutes)
+
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get((req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,()=>{
     connectToMongoDb();
